65759c5ee9950800bcf00b3c9beeaed68bc6282c
Ante Kraljević <ante.kraljevic@hotmail.com>
2021-08-12 16:54:27 +0200

    Clean-up code

--- Controllers/ArmyController.cs
+++ Controllers/ArmyController.cs
@@ -1,11 +1,7 @@
 ﻿using BornfightTask.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BornfightTask.Controllers
 {
