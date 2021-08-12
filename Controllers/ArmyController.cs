4b70f5632849fd386da60ee653fcd03c05436ff8
Ante Kraljević <ante.kraljevic@hotmail.com>
2021-08-12 16:52:38 +0200

    Add project files.

---
+++ Controllers/ArmyController.cs
@@ -0,0 +1,62 @@
+﻿using BornfightTask.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BornfightTask.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ArmyController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> BeginBattle([FromQuery, Required] int army1, [FromQuery, Required] int army2)
+        {
+            if(army1 < 1 || army2 < 1)
+            {
+                return BadRequest("Invalid parameters, armies have to have soldiers.");
+            }
+
+            bool isBattleOver = false;
+            Armies armies = new Armies(army1, army2);
+            
+            List<string> log = new List<string>();
+            log.Add("Battle begins! Army1 has " + army1 + " soldiers. Army2 has" + army2 + " soldiers.");
+
+            int numberOfClashes = 0;
+            while (!isBattleOver)
+            {
+                numberOfClashes++;
+
+                (armies, log) = Armies.Clash(armies, log);
+
+                log.Add("Clash #" + numberOfClashes + " is over. Army1 has " + armies.Army1 + " soldiers remaining. Army2 has " + armies.Army2 + " soldiers remaining.");
+                if (armies.Army1 < 1 || armies.Army2 < 1)
+                {
+                    isBattleOver = true;
+                }
+            }
+
+
+            if (armies.Army2 < 1)
+            {
+                log.Add("Army1 won!");
+                return Ok(log);
+            }  
+            else if (armies.Army1 < 1)
+            {
+                log.Add("Army2 won!");
+                return Ok(log);
+            }
+            else
+            {
+                log.Add("Something bad happened:/");
+                return Ok(log);
+            }
+        }
+    }
+}
---
+++ Models/Armies.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BornfightTask.Models
+{
+    public class Armies
+    {
+        public int Army1 { get; set; }
+        public int Army2 { get; set; }
+
+        public Armies()
+        {
+
+        }
+
+        public Armies(int army1, int army2)
+        {
+            this.Army1 = army1;
+            this.Army2 = army2;
+        }
+
+        public static (Armies, List<string>) Clash(Armies armies, List<string> log)
+        {
+            Random rnd = new Random();
+
+            //earthquake has 10% of chance to happen
+            int earthquakeChance = rnd.Next(1, 101);
+            if(earthquakeChance  < 11)
+            {
+                //earthquake happened, eliminate random number of soldiers from each army
+                log.Add("EARTHQUAKE!");
+                int percentageOfEarthquakeCasualtiesArmy1 = rnd.Next(10, 16);
+                int percentageOfEarthquakeCasualtiesArmy2 = rnd.Next(10, 16);
+                armies = ChopPercentageOfSoldiersFromEachArmy(armies, percentageOfEarthquakeCasualtiesArmy1, percentageOfEarthquakeCasualtiesArmy2);
+            }
+
+            //major conflict is here below, each clash makes each amry lose between 10 and 15 percent of soldiers
+            log.Add("FIGHT!");
+            int percentageOfClashCasualtiesArmy1 = rnd.Next(10, 16);
+            int percentageOfClashCasualtiesArmy2 = rnd.Next(10, 16);
+
+            armies = ChopPercentageOfSoldiersFromEachArmy(armies, percentageOfClashCasualtiesArmy1, percentageOfClashCasualtiesArmy2);
+            return (armies, log);
+        }
+
+        private static Armies ChopPercentageOfSoldiersFromEachArmy(Armies armies, int percentage1, int percentage2)
+        {
+            int casualtiesArmy1 = (percentage1 * armies.Army1) / 100;
+            int casualtiesArmy2 = (percentage2 * armies.Army2) / 100;
+
+            if (armies.Army1 < 10)
+            {
+                Random rnd = new Random();
+                casualtiesArmy1 = rnd.Next(1, 11);
+            }
+
+            if (armies.Army2 < 10)
+            {
+                Random rnd = new Random();
+                casualtiesArmy2 = rnd.Next(1, 11);
+            }
+
+            //we create 2 new armies with casualties applied
+            return new Armies(armies.Army1-casualtiesArmy1, armies.Army2-casualtiesArmy2);
+        }
+    }
+}
